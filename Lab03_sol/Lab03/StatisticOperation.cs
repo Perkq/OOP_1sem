0d1dc4c5c13dd8d89f2b5c9fe211e2eda3c366a8
Perkq <artsynek@gmail.com>
2022-10-03 18:20:37 +0300

    Добавлены методы расширения для моего типа и для типа string

---
+++ Lab03_sol/Lab03/StatisticOperation.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab03
+{
+    static class StatisticOperation
+    {
+        public static double sumElems(Matrix cur)
+        {
+            double sum = 0;
+            for (int i = 0; i < cur.RowsNum; i++)
+            {
+                for (int q = 0; q < cur.ColNum; q++)
+                {
+                    sum += cur[i, q]; 
+                }
+            }
+
+            return sum;
+        }
+
+        public static double minMaxDif(Matrix cur)
+        {
+            double max = cur[0,0];
+            double min = cur[0, 0];
+            for (int i = 0; i < cur.RowsNum; i++)
+            {
+                for (int q = 0; q < cur.ColNum; q++)
+                {
+                    if (cur[i, q] > max) max = cur[i, q];
+                    if (cur[i, q] < min) min = cur[i, q];
+
+                }
+            }
+
+            return max - min;
+        }
+
+        public static int getNumElem(Matrix cur)
+        {
+            return cur.RowsNum * cur.ColNum;
+        }
+
+        
+        public static int getIntSumRow(Matrix cur, int row)
+        {
+            int intSum = 0;
+            for(int i = 0; i < cur.ColNum; i++)
+            {
+
+                if (cur[row, i] % 1 == 0)
+                {
+                    intSum += Convert.ToInt32(cur[row, i]);
+                }
+            }
+
+            return intSum;
+        }
+
+        public static bool getSqareMatr(Matrix cur)
+        {
+            if(cur.RowsNum == cur.ColNum)
+            {
+                return true;
+            } else
+                {
+                    return false;
+                }
+        }
+
+        public static bool checkSymb(string str, char symb)
+        {
+            for(int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == symb) return true;
+            }
+
+            return false;
+        }
+    }
+}
