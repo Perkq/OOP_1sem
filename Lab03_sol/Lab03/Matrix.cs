94ab2e01403a63717e9f26e77d5f716a7ef027ab
Perkq <artsynek@gmail.com>
2022-10-03 15:44:06 +0300

    Добавил перегрузки, основные методы для работы, вложенные классы и объект

---
+++ Lab03_sol/Lab03/Matrix.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab03
+{
+    class Matrix
+    {
+        private int rows;
+        private int columns;
+        public double[,] matrix;
+
+        public int RowsNum
+        {
+            get
+            {
+                return rows;
+            }
+        }
+
+        public int ColNum
+        {
+            get
+            {
+                return columns;
+            }
+        }
+
+        public double this[int numOfRows, int numOfCols]   //индексатор класса
+        {
+            get { return matrix[numOfRows, numOfCols]; }
+
+            set
+            {
+                if (numOfRows <= rows + 1 && numOfCols <= columns + 1)
+                {
+                    matrix[numOfRows, numOfCols] = value;
+                }
+                else
+                {
+                    Console.WriteLine("Массив заполнен");
+                }
+            }
+        }
+
+
+        // Конструктор 
+        public Matrix(int row, int col)
+        {
+            matrix = new double[row, col];
+            rows = row;
+            columns = col;
+        }
+
+        //Методы
+        public static Matrix operator -(Matrix frst, Matrix scnd)
+        {
+            if((frst.rows != scnd.rows) && (frst.columns != scnd.columns))
+            {
+                Console.WriteLine("Ошибка! Размерности матриц не совпадают! Будет возвращена первая матрица");
+                return frst;
+            }
+
+            Matrix temp = new Matrix(frst.rows, frst.columns);
+
+            for(int i = 0; i < frst.rows; i++)
+            {
+                for(int q = 0; q < frst.columns; q++)
+                {
+                    temp[i, q] = frst[i, q] - scnd[i, q];
+                }
+            }
+
+            return temp;
+        }
+
+        public static Matrix operator ++(Matrix current)
+        {
+            for (int i = 0; i < current.rows; i++)
+            {
+                for (int q = 0; q < current.columns; q++)
+                {
+                    current[i, q] += 1; 
+                }
+            }
+
+            return current;
+        }
+
+        public static bool operator !=(Matrix frst, Matrix scnd)
+        {
+            if ((frst.rows != scnd.rows) && (frst.columns != scnd.columns))
+            {
+                Console.WriteLine("Ошибка! Размерности матриц не совпадают!");
+                return false;
+            }
+
+            int count = 0;
+
+            for (int i = 0; i < frst.rows; i++)
+            {
+                for (int q = 0; q < frst.columns; q++)
+                {
+                    if (frst.matrix[i, q] == scnd.matrix[i, q])
+                    {
+                        count++;
+                    };
+                }
+            }
+            if(count < frst.rows * frst.columns)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator ==(Matrix frst, Matrix scnd)
+        {
+            if ((frst.rows != scnd.rows) && (frst.columns != scnd.columns))
+            {
+                Console.WriteLine("Ошибка! Размерности матриц не совпадают!");
+                return false;
+            }
+
+
+            for (int i = 0; i < frst.rows; i++)
+            {
+                for (int q = 0; q < frst.columns; q++)
+                {
+                    if (frst[i, q] != scnd[i, q])
+                    {
+                        return false;
+                    };
+                }
+            }
+
+            return true;
+        }
+
+        public static explicit operator int(Matrix cur)
+        {
+            int count = 0;
+            for (int i = 0; i < cur.rows; i++)
+            {
+                for (int q = 0; q < cur.columns; q++)
+                {
+                    if (cur[i,q] == 0)
+                    {
+                        count++;
+                    };
+                }
+            }
+
+            return count;
+        }
+
+        public class Production
+        {
+            private readonly int id = 123;
+            private readonly string organization = "БГТУ";
+
+            public void getDev()
+            {
+                Console.WriteLine("Id: " + id);
+                Console.WriteLine("Организация: " + organization);
+            }
+        }
+
+        public static Production org = new Production();
+
+        public class Developer
+        {
+            private readonly int id = 777;
+            private readonly string FIO = "Нехаёнок Артём Олегович";
+            private readonly string department = "2-4 ПОИТ";
+
+            public void getDev()
+            {
+                Console.WriteLine("Id: " + id);
+                Console.WriteLine("ФИО: " + FIO);
+                Console.WriteLine("Отдел: " + department);
+
+            }
+        }
+    }
+}
---
+++ Lab03_sol/Lab03/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Lab03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.Write("Введите номер проверки: ");
+            switch(Convert.ToInt32(Console.ReadLine()))
+            {
+                case 1:
+                    Matrix test = new Matrix(4, 4);
+                    Matrix test2 = new Matrix(4, 4);
+
+                    for (int i = 0; i < test.RowsNum; i++)
+                    {
+                        for (int q = 0; q < test.ColNum; q++)
+                        {
+                            test[i, q] = i;
+                            Console.Write(test[i, q] + " ");
+                        }
+                        Console.WriteLine();
+
+                    }
+
+                    Console.WriteLine();
+
+                    for (int i = 0; i < test.RowsNum; i++)
+                    {
+                        for (int q = 0; q < test.ColNum; q++)
+                        {
+                            test2[i, q] = q;
+                            Console.Write(test2[i, q] + " ");
+                        }
+                        Console.WriteLine();
+                    }
+
+                    Matrix minus = test2 - test;
+
+                    Console.WriteLine();
+
+                    for (int i = 0; i < minus.RowsNum; i++)
+                    {
+                        for (int q = 0; q < minus.ColNum; q++)
+                        {
+                            Console.Write(minus[i, q] + " ");
+                        }
+                        Console.WriteLine();
+                    }
+                    break;
+                case 2:
+                    Matrix testInkr = new Matrix(4, 4);
+
+                    for (int i = 0; i < testInkr.RowsNum; i++)
+                    {
+                        for (int q = 0; q < testInkr.ColNum; q++)
+                        {
+                            testInkr[i, q] = i;
+                            Console.Write(testInkr[i, q] + " ");
+                        }
+                        Console.WriteLine();
+
+                    }
+
+                    Console.WriteLine();
+
+                    for (int i = 0; i < testInkr.RowsNum; i++)
+                    {
+                        for (int q = 0; q < testInkr.ColNum; q++)
+                        {
+                            testInkr[i, q]++;
+                            Console.Write(testInkr[i, q] + " ");
+                        }
+                        Console.WriteLine();
+                    }
+                    break;
+                case 3:
+                    Matrix testEq = new Matrix(4, 4);
+                    Matrix testEqSec = new Matrix(4, 4);
+
+
+                    for (int i = 0; i < testEq.RowsNum; i++)
+                    {
+                        for (int q = 0; q < testEq.ColNum; q++)
+                        {
+                            testEq[i, q] = i;
+                            testEqSec[i, q] = q;
+                        }
+
+                    }
+                    Console.WriteLine(testEq != testEqSec);
+                    Console.WriteLine(testEq == testEqSec);
+                    break;
+                case 4:
+                    Matrix testInt = new Matrix(4, 4);
+
+                    for (int i = 0; i < testInt.RowsNum; i++)
+                    {
+                        for (int q = 0; q < testInt.ColNum; q++)
+                        {
+                            testInt[i, q] = q;
+                            Console.Write(testInt[i, q] + " ");
+                        }
+                        Console.WriteLine();
+
+                    }
+                    Console.WriteLine((int)testInt);
+                    break;
+                case 5:
+                    Matrix.Developer testDev = new Matrix.Developer();
+                    testDev.getDev();
+                    Matrix.org.getDev();
+                    break;
+            }
+        }
+    }
+}
