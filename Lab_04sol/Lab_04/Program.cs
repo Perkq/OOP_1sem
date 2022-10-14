c0bceb9c5a2a2b3536f10868b52ed443841641a0
Perkq <artsynek@gmail.com>
2022-10-14 15:59:25 +0300

    Демонстрация работоспособности

--- Lab_04sol/Lab_04/Program.cs
+++ Lab_04sol/Lab_04/Program.cs
@@ -13,7 +13,31 @@ namespace Lab_04
             Ad test = new Ad(288, "2 month");
 
             Printer print = new Printer();
-            Object secr = new News("порно", "10 years");
+            Object secr = new News("Криминал", "10 years");
+
+            Film testFilm = new Film("Топчик", "Мужик");
+
+            object polimorph = testFilm as Producer;
+            if(polimorph != null)
+            {
+                Console.WriteLine(print.IAmPrinting(polimorph));
+            }
+            else
+            {
+                Console.WriteLine("Операция невозможна");
+            }
+
+            Ad testAd = new Ad(552, "one month");
+
+            object testPolimAd = testAd as TVProgram;
+            if (testPolimAd is TVProgram)
+            {
+                Console.WriteLine(print.IAmPrinting(testPolimAd));
+            }
+            else
+            {
+                Console.WriteLine("Операция невозможна");
+            }
 
             Console.WriteLine(print.IAmPrinting(secr));
         }
