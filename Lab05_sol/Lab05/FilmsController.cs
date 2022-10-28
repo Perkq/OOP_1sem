ad95fff0683b312e90ac497c58b7d901d67eea46
Perkq <artsynek@gmail.com>
2022-10-28 16:37:08 +0300

    Контейнер и контроллер для фильмов

---
+++ Lab05_sol/Lab05/FilmsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab05
+{
+    class FilmsController : FilmsCont
+    { 
+        public void searchFilms(string curDate)
+        {
+            foreach(Producer temp in filmsList)
+            {
+                Film Film = temp as Film;
+                if(Film.Date == curDate)
+                {
+                    Console.WriteLine(Film.ToString());
+                }
+            }
+        }
+    }
+}
