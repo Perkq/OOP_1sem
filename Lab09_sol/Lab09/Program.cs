376c48e6e913ae33f85c910b5b141ba688cbe66c
Perkq <artsynek@gmail.com>
2022-11-14 03:41:59 +0300

    Custom collection + universal collections

---
+++ Lab09_sol/Lab09/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arr = new ArrayList();
+            arr.Add(22);
+            arr.Add("lol");
+            arr.Add(23.5);
+            arr.Add('q');
+            arr.Add(true);
+
+            
+
+            Stack st = new Stack(arr);
+
+        }
+    }
+}
